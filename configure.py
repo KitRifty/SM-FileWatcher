73413e8b1445383c63fed4029420a4f7098caa3c
KitRifty <kitrifty@gmail.com>
2022-08-05 22:07:29 -0700

    Add enable auto versioning

--- configure.py
+++ configure.py
@@ -20,6 +20,8 @@ parser.options.add_argument('--enable-debug', action='store_const', const='1', d
                        help='Enable debugging symbols')
 parser.options.add_argument('--enable-optimize', action='store_const', const='1', dest='opt',
                        help='Enable optimization')
+parser.options.add_argument('--enable-auto-versioning', action='store_false', dest='disable_auto_versioning',
+                       default=True, help='Enables the auto versioning script')
 parser.options.add_argument('--targets', type=str, dest='targets', default=None,
                        help='Override the target architecture (use commas to separate multiple targets).')
 parser.Configure()
